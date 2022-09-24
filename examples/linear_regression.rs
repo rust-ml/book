fa506bbb565e480855edb7cc83f64b2d9d1029d4
chrism <christopher.and.moran@gmail.com>
2022-09-24 18:47:41 -0400

    Add x_lim, y_lim to linear regression code

--- examples/linear_regression.rs
+++ examples/linear_regression.rs
@@ -1,23 +1,28 @@
 use linfa::prelude::*;
-use linfa_linear::LinearRegression;
+use linfa_linear::{LinearRegression, TweedieRegressor};
 use ndarray::prelude::*;
+use ndarray_stats::QuantileExt;
 use plotters::prelude::*;
 
 fn main() {
-    let array: Array2<f64> = linfa_book::create_line(1.0, 2.0, 50, [0., 7.]).mapv(|x| x as f64);
+    // let array: Array2<f64> = linfa_book::create_line(1.0, 2.0, 50, [0., 7.]).mapv(|x| x as f64);
+    let array: Array2<f64> = linfa_book::create_curve(1.0, 1.0, 0.0, 50, [0.0, 7.0]);
 
     // Converting from an array to a Linfa Dataset can be the trickiest part of this process
     let (data, targets) = (
         array.slice(s![.., 0..1]).to_owned(),
         array.column(1).to_owned(),
     );
+    let x_max = data.max().unwrap().ceil();
+    let y_max = targets.max().unwrap().ceil();
 
     let dataset = Dataset::new(data, targets).with_feature_names(vec!["x", "y"]);
 
     let lin_reg = LinearRegression::new();
     let model = lin_reg.fit(&dataset).unwrap();
 
-    let root_area = BitMapBackend::new("plots/linear.png", (600, 400)).into_drawing_area();
+    let root_area =
+        BitMapBackend::new("target/linear_regression.png", (600, 400)).into_drawing_area();
     root_area.fill(&WHITE).unwrap();
 
     let mut ctx = ChartBuilder::on(&root_area)
@@ -25,7 +30,7 @@ fn main() {
         .set_label_area_size(LabelAreaPosition::Bottom, 40)
         .caption("Legend", ("sans-serif", 40))
         .caption("Linear Regression", ("sans-serif", 40))
-        .build_cartesian_2d(0.0..8f64, 0.0..10f64)
+        .build_cartesian_2d(0.0..x_max, 0.0..y_max)
         .unwrap();
 
     ctx.configure_mesh().draw().unwrap();
